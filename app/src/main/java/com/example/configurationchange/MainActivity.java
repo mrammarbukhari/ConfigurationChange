7b6e0883945cb1ca65eb835d46143d0173adc1ec
mrammarbukhari <74782635+mrammarbukhari@users.noreply.github.com>
2021-04-08 19:07:49 +0500

    Add files via upload

---
+++ app/src/main/java/com/example/configurationchange/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.configurationchange;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void ShowToast(View view) {
+        Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
+    }
+}
